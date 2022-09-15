6f66cc25ac13db8bde50c0b364c0b0dd5f995287
Mamokgolwane Moloi <108424497+mamsieCodess@users.noreply.github.com>
2022-09-15 20:21:16 +0200

    Full button functionalities
    Added functions that will make the edit, delete and add buttons to edit, delete and add the tasks from the DOM when they are clicked.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const taskName = document.getElementById('nameInput');
 const msg = document.getElementById('error-msg');
 const dueDate = document.getElementById('dateInput');
 const submit = document.querySelector('.inputs form #add1');
-const tasks = document.querySelector('.tasks');
+const tasksList = document.querySelector('.tasksList');
 
 //created varaiables
 let todos = [] //for function saveTask
@@ -16,7 +16,7 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault();
 
     saveTask(); //when the submit button is clicked, a todo is saved
-   
+    renderTask(); //after saving a task, it must be displayed on the DOM
 })
 
 //Saving a new task
@@ -32,11 +32,12 @@ function saveTask(){ //each task is saved in an array called todoList
     const duplicatedEntry = todos.some((todo)=> todo.value1.toUpperCase() === todoValue1.toUpperCase()); //if the value the user is trying to enter is the same one as the one saved in the object
 
 
-    if(emptyField){
-        alert('Field is empty')
+    if(emptyField){ 
+        msg.style.visibility = 'visible' //make error message visible
     }
     else if(duplicatedEntry){
-        alert('Task exists')
+        msg.innerText = 'OOPS! Such a task already exists.'
+        msg.style.visibility = 'visible';
     }
     else
     { //if the field is not then save a new task to the array
@@ -44,11 +45,62 @@ function saveTask(){ //each task is saved in an array called todoList
         todos.push({ //saving(pushing) a new created task in an object(todo) called todos (array)
             value1 : todoValue1,
             value2 :todoValue2,
-            checked: false //the todo is false for the first time it is created
         });
+
+        msg.style.visibility = 'hidden'
+    }
+
         taskName.value = ''; //clears the field to be empty after submitting
         dueDate.value = '' //clears the field to be empty after submitting
+ 
     }
+
+
+
+//to give a created task to the DOM and display it
+function renderTask(){
+    //clear the DOM before rendering to avaoid duplication
+    tasksList.innerHTML = '';
+    //then render the crteaed tasks
+    todos.forEach((todo,index)=>{ //loop over the array and for each created task, create it in the following structure and attach it to the tasks div
+        tasksList.innerHTML += ` 
+        <div class="newTask id='${index}' ">
+          <span id="dueDate">${todo.value2}</span>
+          <p id="taskName">${todo.value1}</p>
+
+          <div class="options">
+            <span><i onclick='completedTask(this)' class="fa-regular fa-circle"></i></span>
+            <span><i onClick= 'editTask(this)'  class="fa-sharp fa-solid fa-pen-to-square completeButton" data-action="edit"></i></span>
+            <span><i onClick ='deleteTask(this)' class="fa-sharp fa-solid fa-trash" data-action="delete"></i></span>
+          </div>
+        </div>
+        `
+    })
 }
 
 
+//edit button functionality
+let editTask = (e) => {
+    let selectedItem = e.parentElement.parentElement.parentElement;
+
+    dueDate.value = selectedItem.children[0].innerHTML;
+    taskName.value = selectedItem.children[1].innerHTML;
+    deleteTask(e);
+
+}
+//delete button functionality
+
+let deleteTask = (e) => {
+    e.parentElement.parentElement.parentElement.remove();
+    todos.splice(e.parentElement.parentElement.parentElement.id, 1);
+
+    // re-render
+  renderTask();
+}
+
+//edit buttton functionality
+let completedTask = (e) =>{
+    let parentItem = e.parentElement.parentElement.parentElement;
+    parentItem.classList.toggle('completed');
+}
+///*** 
