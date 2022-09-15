4333e3436ff32f6ada2451ad035f3da91bcdfef5
Mamokgolwane Moloi <108424497+mamsieCodess@users.noreply.github.com>
2022-09-15 08:23:36 +0200

    Linking HTML elements to Javascript
    Successfully linked HTML elements to the script by assigning them to variables.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+//selectors
+
+const form = document.querySelector('.inputs form');
+const taskName = document.getElementById('nameInput');
+const msg = document.getElementById('error-msg');
+const dueDate = document.getElementById('dateInput');
+const submit = document.querySelector('.inputs form #add1');
+const tasks = document.querySelector('.tasks');
+
 
